41b0a7bdd3bd0af93cada1e4dbdc3b59f5dbb4f3
imahdimir <imahdimir@gmail.com>
2022-10-26 19:16:17 +0330

    + githubb.ret_fps_with_suf_in_github_repo()

--- src/giteasy/githubb.py
+++ src/giteasy/githubb.py
@@ -24,12 +24,10 @@ def get_token(github_usr = None) :
     tok = input('enter github access token:')
     return tok
 
-
 def ret_usr_repo_from_repo_url(repo_url) :
     """ Return the user/repo name from a GitHub repository URL. """
     rp = Repo(repo_url)
     return rp.user_repo
-
 
 def ret_pygithub_github_obj(tok = None , github_usr = None) :
     """ Return a PyGitHub GitHub object. """
@@ -38,12 +36,10 @@ def ret_pygithub_github_obj(tok = None , github_usr = None) :
     g = Github(tok)
     return g
 
-
 def ret_pygithub_repo_obj(usr_repo , tok = None , github_usr = None) :
     """ Return a PyGitHub repo object. """
     g = ret_pygithub_github_obj(tok , github_usr)
     return g.get_repo(usr_repo)
-
 
 def _get_all_fps_in_repo(pygithub_repo_obj , sha = None , recursive = False) :
     """ Get all files sha & pathes in a GitHub repository. """
@@ -53,20 +49,17 @@ def _get_all_fps_in_repo(pygithub_repo_obj , sha = None , recursive = False) :
         sha = rp.get_branch(br).commit.sha
     return rp.get_git_tree(sha = sha , recursive = recursive).tree
 
-
 def get_all_fps_in_repo(repo_url , sha = None , recursive = False) :
     """ Get all files sha & pathes in a GitHub repository. """
     ur = ret_usr_repo_from_repo_url(repo_url)
     rp = ret_pygithub_repo_obj(ur)
     return _get_all_fps_in_repo(rp , sha , recursive)
 
-
 def find_file_sha(repo_url , fn) :
     """ Find the sha of a file in a GitHub repository. by its name. """
     ur = ret_usr_repo_from_repo_url(repo_url)
     rp = ret_pygithub_repo_obj(ur)
     return _find_file_sha(rp , fn)
-
 
 def _find_file_sha(pygithub_repo_obj , fn) :
     """ Find the sha of a file in a GitHub repository. """
@@ -75,7 +68,6 @@ def _find_file_sha(pygithub_repo_obj , fn) :
     for _fn in fns :
         if _fn.path == fn :
             return _fn.sha
-
 
 def _add_overwrite_a_file_2_repo(fp ,
                                  pygithub_repo_obj ,
@@ -115,14 +107,12 @@ def _add_overwrite_a_file_2_repo(fp ,
 
         print(_ms , f' 2  {rp.full_name}')
 
-
 def add_overwrite_a_file_2_repo(fp , repo_url , msg = None , branch = 'main') :
     """ Add a text based file to a GitHub repository. If the file already exists, it will be overwritten. """
     ur = ret_usr_repo_from_repo_url(repo_url)
     rp = ret_pygithub_repo_obj(ur)
     fu = _add_overwrite_a_file_2_repo
     fu(fp , rp , msg , branch)
-
 
 def _find_new_files_fr_dir_not_in_repo_by_suf(dirpath ,
                                               file_suf ,
@@ -147,12 +137,10 @@ def _find_new_files_fr_dir_not_in_repo_by_suf(dirpath ,
 
     return nfns
 
-
 def find_stems_fr_dir_not_in_repo(dirpath , file_suf , repo_url) :
     ur = ret_usr_repo_from_repo_url(repo_url)
     rp = ret_pygithub_repo_obj(ur)
     return _find_new_files_fr_dir_not_in_repo_by_suf(dirpath , file_suf , rp)
-
 
 def add_overwrite_files_by_suf_fr_dir_2_repo(dirpath ,
                                              file_suf ,
@@ -174,12 +162,10 @@ def add_overwrite_files_by_suf_fr_dir_2_repo(dirpath ,
         fps = [_dir / x for x in fns]
         _ = [fu(x , rp) for x in fps]
 
-
 @dataclass
 class RRetFpsPyGithubRepoObj :
     fps: list
     pygithub_repo_obj: object
-
 
 def ret_fps_and_pygithub_repo_obj(dirpath ,
                                   file_suf ,
@@ -200,7 +186,6 @@ def ret_fps_and_pygithub_repo_obj(dirpath ,
 
     ro = RRetFpsPyGithubRepoObj
     return ro(fps = fps , pygithub_repo_obj = rp)
-
 
 def upload_files_by_suf_from_dir_2_repo_mp(dirpath ,
                                            file_suf ,
@@ -229,7 +214,6 @@ def upload_files_by_suf_from_dir_2_repo_mp(dirpath ,
 
     return len(fps)
 
-
 def persistently_upload_files_from_dir_2_repo_mp(dirpath ,
                                                  file_suf ,
                                                  repo_url ,
@@ -256,8 +240,14 @@ def persistently_upload_files_from_dir_2_repo_mp(dirpath ,
         except KeyboardInterrupt :
             break
 
-
 def get_github_rate_limit(tok = None , github_usr = None) :
     """ Get the GitHub API rate limit. """
     gh = ret_pygithub_github_obj(tok = tok , github_usr = github_usr)
     return gh.get_rate_limit()
+
+def ret_fps_with_suf_in_github_repo(repo_name , file_suffix) :
+    """ returns a list of files havig file_suffix in the GitHub repo """
+    fps = get_all_fps_in_repo(repo_name)
+    fps = [Path(x.path) for x in fps]
+    fps_wt_suf = [x for x in fps if x.suffix == file_suffix]
+    return fps_wt_suf

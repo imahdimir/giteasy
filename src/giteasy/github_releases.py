f5c742602d78a1c2bddb52d1d2536dbac1bb38c6
imahdimir <imahdimir@gmail.com>
2022-11-04 18:37:08 +0330

    v9.4.0, + github_releases.get_tar_url_of_latest_release_of_a_public_github_repo()

---
+++ src/giteasy/github_releases.py
@@ -0,0 +1,15 @@
+"""
+
+    """
+
+import requests
+
+from .github_repo import GitHubRepo
+
+
+def get_tar_url_of_latest_release_of_a_public_github_repo(repo_url) :
+    ghr = GitHubRepo(repo_url)
+    url = f'https://api.github.com/repos/{ghr.usr_repo}/releases/latest'
+    r = requests.get(url)
+    dct = r.json()
+    return dct['tarball_url']
